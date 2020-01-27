370b334d13330a711cc7b3bf186592fab7bcbcad
saad <marva.saad2@gmail.com>
2020-01-27 11:43:34 +0100

    Initial Commit

---
+++ MyFirstNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstNavigation
+//
+//  Created by saad  on 27/01/2020.
+//  Copyright © 2020 saad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
